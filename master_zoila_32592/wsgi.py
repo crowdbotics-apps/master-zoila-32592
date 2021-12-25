92ad9bf614889b0141d114df46baaa65beb14fdb
Crowdbotics <team@crowdbotics.com>
2021-12-25 20:09:18 +0000

    Initial commit.

---
+++ master_zoila_32592/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for master_zoila_32592 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'master_zoila_32592.settings')
+
+application = get_wsgi_application()
